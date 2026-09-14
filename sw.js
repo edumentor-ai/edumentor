487c99d06fdf03f850c653cf7740f10326594860
edumentor-ai <umar81828385@gmail.com>
2026-09-14 11:57:43 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+// EduMentor AI Service Worker
+const CACHE_NAME = 'edumentor-v1';
+const assetsToCache = [
+  '/edumentor/',
+  '/edumentor/index.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
